c4aeb8124428c183d09e8565f3f8f04a741c6287
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-18 13:38:44 +0300

    set the cash

--- src/run_backtrader.py
+++ src/run_backtrader.py
@@ -5,6 +5,7 @@ import backtrader as bt
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000.0)
 
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
